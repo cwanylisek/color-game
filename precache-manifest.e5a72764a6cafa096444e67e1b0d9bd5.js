6d35116ab98bf4f892a1b7fa74c2e69dd119193f
root <rewiks@gmail.com>
2019-12-04 22:42:10 +0100

    Updates

---
+++ precache-manifest.e5a72764a6cafa096444e67e1b0d9bd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5876cbcfb5e175788b33",
+    "url": "/color-game/static/js/runtime~main.5876cbcf.js"
+  },
+  {
+    "revision": "484a08ce065202583131",
+    "url": "/color-game/static/js/main.484a08ce.chunk.js"
+  },
+  {
+    "revision": "0dac2f1e807326235815",
+    "url": "/color-game/static/js/1.0dac2f1e.chunk.js"
+  },
+  {
+    "revision": "484a08ce065202583131",
+    "url": "/color-game/static/css/main.116164e1.chunk.css"
+  },
+  {
+    "revision": "523ff8822508a3dff259f58071c037b4",
+    "url": "/color-game/index.html"
+  }
+];
